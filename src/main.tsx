ff4ed246d44d363bc6919daaeb8647ca8529c10e
Tomas Gonzalez <tomimateo2008@hotmail.com>
2024-07-01 00:52:44 -0300

    actualizacion v3

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+
 import { NextUIProvider } from "@nextui-org/react";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <NextUIProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </NextUIProvider>
   </React.StrictMode>
 );
