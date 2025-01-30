ce87b7132ee002b988b939f083d10a80e86f7c0f
Tomas Gonzalez <tomimateo2008@hotmail.com>
2025-01-30 19:28:37 -0300

    actualizacion

--- src/components/ui/skeleton/RepoCountSkeleton.tsx
+++ src/components/ui/skeleton/RepoCountSkeleton.tsx
@@ -1,7 +1,7 @@
 export const RepoCountSkeleton = () => {
   return (
-    <div className="animate-pulse">
-      <div className="bg-gray-500 rounded-md w-full"></div>
+    <div className="animate-pulse w-20 h-4">
+      <div className="bg-gray-500 rounded-md w-full h-full"></div>
     </div>
   );
 };
