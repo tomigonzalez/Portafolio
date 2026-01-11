447e89e9abe93e60ca37ca03784d324bb6a2db25
tomi <tomimateo2008@hormail.com>
2026-01-11 18:14:25 -0300

    changes

--- src/data/experiencia.ts
+++ src/data/experiencia.ts
@@ -1,6 +1,24 @@
 const experienciaData = [
   {
-    area: "SISTEMAS",
+    area: "Front end",
+    techonolgies: "React, CSS, Docker, Git, HTML, JavaScript.",
+    descrpcion:
+      "Actualmente me desempeño como Desarrollador Front-end, participando en el diseño, desarrollo y optimización de la plataforma web de Gestar Educativa. Trabajo principalmente con React y otras tecnologías modernas para crear interfaces eficientes, intuitivas y responsivas, contribuyendo a mejorar la experiencia de usuarios y estudiantes. Colaboro en la implementación de nuevas funcionalidades, optimización de rendimiento y mejoras en UX/UI, asegurando que la plataforma sea atractiva, funcional y consistente en todos los dispositivos.",
+    age: "2025-Actual",
+    link: "https://gestareducativa.com.ar/",
+    from: "Gestar Educativa",
+  },
+  {
+    area: "Full Stack",
+    techonolgies: "HTML, CSS, JavaScript, Wordpress, PHP , Laravel.",
+    descrpcion:
+      " Me desempeñe en el area de sistemas en {NASPERU}. Desarrollando sitios web utilizando diferentes temas de Wordpress y los modifico/personalizo utilizando HTML, CSS y JavaScript. Ademas mejore mis habilidades en PHP Y Laravel. ",
+    age: "2024-2025",
+    link: "https://www.nasperu.com/",
+    from: "NASPERU",
+  },
+  {
+    area: "Full Stack",
     techonolgies:
       "React, HTML, CSS, Typescript, Node.js, Express.js, MongoDB, MySQL, WordPress. ",
     descrpcion:
@@ -9,15 +27,8 @@ const experienciaData = [
     link: "#",
     from: "FREELANCE",
   },
-  {
-    area: "SISTEMAS",
-    techonolgies: "HTML, CSS, JavaScript, Wordpress, PHP , Laravel.",
-    descrpcion:
-      " Actualmente me desempeño en el area de sistemas en {NASPERU}. Desrrollo sitios web utilizando diferentes temas de Wordpress y los modifico/personalizo utilizando HTML, CSS y JavaScript. Ademas estoy mejorando mis habilidades en PHP Y Laravel. ",
-    age: "2024-Actual",
-    link: "https://www.nasperu.com/",
-    from: "NASPERU",
-  },
+  
+  
 ];
 
 export default experienciaData;
